6200fe69e29b0ced5fb2a47a3260be7022549815
BrownOdyssey <83645833+chldudqlsdl@users.noreply.github.com>
2024-05-05 20:41:55 +0900

    feat: 프로젝트 생성

---
+++ ODindi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ODindi
+//
+//  Created by Youngbin Choi on 5/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
