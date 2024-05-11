7a04e4ef9ec48774c783f7476f16a33a9297c709
BrownOdyssey <83645833+chldudqlsdl@users.noreply.github.com>
2024-05-11 23:41:08 +0900

    feat: 컬렉션뷰에 영화관 날짜 바인딩, 영화관 뷰 클릭하면 날짜 바뀌는 기능, 데이터 로딩시 activityIndicator 추가

--- ODindi/View/MainViewController.swift
+++ ODindi/View/MainViewController.swift
@@ -18,16 +18,22 @@ class MainViewController: UIViewController {
     // MARK: - Nested Type
     private enum Section {
         case cinema
+        case date
     }
     typealias CinemaItem = IndieCinema
+    typealias DateItem = String
     
     // MARK: - Properties
     
     var viewModel: MainViewModelType
     var disposeBag = DisposeBag()
     private var cinemaCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
+    private var dateCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
     private var cinemaDataSource: UICollectionViewDiffableDataSource<Section, CinemaItem>!
-    private var snapshot: NSDiffableDataSourceSnapshot<Section, CinemaItem>!
+    private var dateDataSource: UICollectionViewDiffableDataSource<Section, DateItem>!
+    private var cinemaSnapshot: NSDiffableDataSourceSnapshot<Section, CinemaItem>!
+    private var dateSnapshot: NSDiffableDataSourceSnapshot<Section, DateItem>!
+    private var activityIndicator = UIActivityIndicatorView()
     
     // MARK: - LifeCycle Methods
     init(viewModel: MainViewModelType = MainViewModel()) {
@@ -50,27 +56,51 @@ class MainViewController: UIViewController {
     private func configureUI() {
         configureAutoLayout()
         configureCollectionView()
-        cinemaCollectionView.backgroundColor = .secondarySystemBackground
+        cinemaCollectionView.backgroundColor = .orange
+        dateCollectionView.backgroundColor = .orange
     }
     
     private func configureAutoLayout() {
         view.addSubview(cinemaCollectionView)
         cinemaCollectionView.snp.makeConstraints {
-            $0.width.height.equalToSuperview()
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.width.equalToSuperview()
+            $0.height.equalTo(50)
+        }
+        view.addSubview(dateCollectionView)
+        dateCollectionView.snp.makeConstraints {
+            $0.top.equalTo(cinemaCollectionView.snp.bottom).offset(5)
+            $0.width.equalToSuperview()
+            $0.height.equalTo(50)
+        }
+        view.addSubview(activityIndicator)
+        activityIndicator.snp.makeConstraints {
+            $0.top.equalTo(cinemaCollectionView.snp.bottom).offset(5)
+            $0.width.equalToSuperview()
+            $0.height.equalTo(50)
         }
     }
     
     private func configureCollectionView() {
         cinemaCollectionView.collectionViewLayout = configureCollectionViewLayout()
         configureCellRegisterationAndDataSource()
+        dateCollectionView.collectionViewLayout = configureCollectionViewLayout()
+        cinemaCollectionView.isScrollEnabled = false
+        dateCollectionView.isScrollEnabled = false
     }
     
     private func configureCellRegisterationAndDataSource() {
         cinemaCollectionView.register(CinemaCell.self, forCellWithReuseIdentifier: "CinemaCell")
+        dateCollectionView.register(CinemaCell.self, forCellWithReuseIdentifier: "CinemaCell")
         
         cinemaDataSource = UICollectionViewDiffableDataSource(collectionView: cinemaCollectionView, cellProvider: { collectionView, indexPath, item in
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CinemaCell", for: indexPath) as? CinemaCell else { return nil}
             cell.name = item.name
+            return cell
+        })
+        dateDataSource = UICollectionViewDiffableDataSource(collectionView: dateCollectionView, cellProvider: { collectionView, indexPath, item in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CinemaCell", for: indexPath) as? CinemaCell else { return nil}
+            cell.name = item
             return cell
         })
     }
@@ -79,7 +109,7 @@ class MainViewController: UIViewController {
         let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
         item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 5, bottom: 0, trailing: 5)
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.4), heightDimension: .fractionalHeight(0.1))
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.4), heightDimension: .fractionalHeight(1))
         let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
         let section = NSCollectionLayoutSection(group: group)
         section.orthogonalScrollingBehavior = .continuous
@@ -95,16 +125,48 @@ class MainViewController: UIViewController {
         viewModel.nearCinemas
             .observe(on: MainScheduler.instance)
             .subscribe { [weak self] items in
-                self?.setSnapShot(items)
+                self?.setCinemaSnapShot(items)
+            }
+            .disposed(by: disposeBag)
+        
+        cinemaCollectionView.rx.itemSelected.asObservable()
+            .map { $0.row }
+            .bind(to: viewModel.didCinemaSelected)
+            .disposed(by: disposeBag)
+        
+        viewModel.selectedCinemaCalendar
+            .map { $0.businessDays }
+            .observe(on: MainScheduler.instance)
+            .subscribe { [weak self] items in
+                self?.setDataSnapshot(items)
+            }
+            .disposed(by: disposeBag)
+        
+        viewModel.isLoading
+            .observe(on: MainScheduler.instance)
+            .subscribe { [weak self] isLoading in
+                if isLoading {
+                    self?.activityIndicator.isHidden = false
+                    self?.activityIndicator.startAnimating()
+                } else {
+                    self?.activityIndicator.isHidden = true
+                    self?.activityIndicator.stopAnimating()
+                }
             }
             .disposed(by: disposeBag)
     }
     
-    func setSnapShot( _ items: [CinemaItem]) {
-        snapshot = NSDiffableDataSourceSnapshot<Section, CinemaItem>()
-        snapshot.appendSections([.cinema])
-        snapshot.appendItems(items, toSection: .cinema)
-        cinemaDataSource.apply(snapshot)
+    func setCinemaSnapShot(_ items: [CinemaItem]) {
+        cinemaSnapshot = NSDiffableDataSourceSnapshot<Section, CinemaItem>()
+        cinemaSnapshot.appendSections([.cinema])
+        cinemaSnapshot.appendItems(items, toSection: .cinema)
+        cinemaDataSource.apply(cinemaSnapshot)
+    }
+    func setDataSnapshot(_ items: [String]) {
+        dateSnapshot = NSDiffableDataSourceSnapshot<Section, DateItem>()
+        dateSnapshot.appendSections([.date])
+        dateSnapshot.appendItems(items, toSection: .date)
+        dateDataSource.apply(dateSnapshot)
     }
 }
 
--- ODindi/ViewModel/MainViewModel.swift
+++ ODindi/ViewModel/MainViewModel.swift
@@ -13,8 +13,12 @@ protocol MainViewModelType {
     var currentCoordinate: Observable<CLLocationCoordinate2D> { get }
     
     var fetchNearCinemas: AnyObserver<Void> { get }
+    var didCinemaSelected: AnyObserver<Int> { get }
     
     var nearCinemas: Observable<[IndieCinema]> { get }
+    var selectedCinemaCalendar: Observable<CinemaCalendar> { get }
+    
+    var isLoading: Observable<Bool> { get }
 }
 
 class MainViewModel: MainViewModelType {
@@ -23,21 +27,28 @@ class MainViewModel: MainViewModelType {
     
     // INPUT
     var fetchNearCinemas: AnyObserver<Void>
+    var didCinemaSelected: AnyObserver<Int>
     
     // OUTPUT
     var nearCinemas: Observable<[IndieCinema]>
+    var selectedCinemaCalendar: Observable<CinemaCalendar>
+    var isLoading: Observable<Bool>
     
     init(_ currentCoordinate: CLLocationCoordinate2D = CLLocationCoordinate2D()){
         self.currentCoordinate = Observable.just(currentCoordinate)
         
         let fetchingNearCinemas = PublishSubject<Void>()
+        let fetchingSelectedCinemaCalendar = BehaviorSubject<Int>(value: 0)
         
         let tempNearCinemas = BehaviorSubject<[IndieCinema]>(value: [])
+        let tempSelectedCinemaCalendar = BehaviorSubject<CinemaCalendar>(value: CinemaCalendar())
+        let loading = BehaviorSubject<Bool>(value: false)
         
         // INPUT
         fetchNearCinemas = fetchingNearCinemas.asObserver()
         
         fetchingNearCinemas
+            .do(onNext: { _ in loading.onNext(true) })
             .withLatestFrom(self.currentCoordinate)
             .subscribe(onNext: { currentCoordinate in
                 let sortedCinemaListByDistance = IndieCinema.list.sorted { cinema1, cinema2 in
@@ -45,16 +56,35 @@ class MainViewModel: MainViewModelType {
                     let distance2 = currentCoordinate.distance(to: cinema2.location)
                     return distance1 < distance2
                 }
-                print(currentCoordinate)
-                print(sortedCinemaListByDistance)
                 tempNearCinemas.onNext(Array(sortedCinemaListByDistance.prefix(3)))
             })
             .disposed(by: disposeBag)
         
-    
+        didCinemaSelected = fetchingSelectedCinemaCalendar.asObserver()
+        
+        fetchingSelectedCinemaCalendar
+            .withLatestFrom(tempNearCinemas) { index, cinemas -> IndieCinema? in
+                guard !cinemas.isEmpty else {
+                    return nil
+                }
+                return cinemas[index]
+            }
+            .flatMap { cinema in
+                guard let cinema = cinema else { return Observable<CinemaCalendar>.empty() }
+                return CinemaService.shared.fetchCinemaCalendar(cinema: cinema)
+            }
+            .subscribe(on: ConcurrentDispatchQueueScheduler(qos: .background))
+            .do(onNext: { _ in loading.onNext(false) })
+            .subscribe(onNext: { calendar in
+                print(calendar.businessDays)
+                tempSelectedCinemaCalendar.onNext(calendar)
+            })
+            .disposed(by: disposeBag)
             
         
         // OUTPUT
         nearCinemas = tempNearCinemas
+        selectedCinemaCalendar = tempSelectedCinemaCalendar
+        isLoading = loading.distinctUntilChanged()
     }
 }
